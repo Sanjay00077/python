f4fed5f8e57760d2bf95f154d5b0d04ed1185318
Sanjay00077 <sanjayvhss@gmail.com>
2026-06-02 12:36:14 +0530

    Add files via upload

---
+++ john.java
@@ -0,0 +1,15 @@
+class john
+{
+public static void main(String[] args) {
+int num1 = 16;
+int num2 = 20;
+int hcf = 1;
+for (int i=1; i<=num1 && i<=num2; i++)
+{
+if(num1%i==0 && num2%i==0 ){
+hcf = i;
+}
+}
+System.out.println("HCF ="+hcf);
+}
+}
